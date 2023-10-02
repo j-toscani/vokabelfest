373a057c16d041f4559ad5c72aefcd7bc4286cf3
julian toscani <julian_toscani@gmx.de>
2023-10-02 18:06:42 +0200

    feat: only provide published tests

--- src/api/vocabulary-test/controllers/vocabulary-test.ts
+++ src/api/vocabulary-test/controllers/vocabulary-test.ts
@@ -4,4 +4,15 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::vocabulary-test.vocabulary-test');
+export default factories.createCoreController('api::vocabulary-test.vocabulary-test', ({ strapi }) =>  ({
+    async findOne(ctx) {
+        const { data, meta } = await super.findOne(ctx);
+        if (!data.attributes.publishedAt) {
+            return { data: null, meta: {} }
+        }
+
+        return {
+            data, meta
+        }
+    }
+}));
